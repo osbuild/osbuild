7f0738a839f8fabd25cd34a50b1c574ff7a7f276
Lars Karlitski <lars@karlitski.net>
2019-07-05 20:08:39 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+import glob
+import setuptools
+
+setuptools.setup(
+    name="osbuild",
+    version="1",
+    description="A build system for OS images",
+    py_modules=["osbuild"],
+    data_files=[
+        ("/usr/bin", ["osbuild"]),
+        ("/usr/lib/osbuild/stages", glob.glob("./stages/*")),
+        ("/usr/lib/osbuild/assemblers", glob.glob("./assemblers/*"))
+    ]
+)
