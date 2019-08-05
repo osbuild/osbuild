5f54832d0efb03980844685618e356bfb8e41a37
Hervé Beraud <hberaud@redhat.com>
2019-08-05 18:24:34 +0200

    Improve packaging and deployment
    - automatically retrieve version from scm version (git tag in our case)
    - deploy on pypi triggered by git tag push on github
    - prefer using setup.cfg instead of setup.py to reduce python code
    - improve packages classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
-import glob
 import setuptools
 
 setuptools.setup(
-    name="osbuild",
-    version="1",
-    description="A build system for OS images",
-    packages=["osbuild"],
-    license='Apache-2.0',
-    entry_points={
-        "console_scripts": ["osbuild = osbuild.__main__:main"]
-    },
+    use_scm_version=True,
 )
