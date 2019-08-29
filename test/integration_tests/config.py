ba846aafcfb3b666afe2e72b6a79b01a73e5078a
Tom Gundersen <teg@jklm.no>
2019-08-29 14:33:17 +0200

    test: test the in-tree version of our tool
    Make sure we test the version of osbuild in the current checkout,
    rather than the system instance.
    
    Also default to using in-place directories for the object store
    and output images. Using a tmpfs does not scale, especially on
    CI infrastructue with limited memory.
    
    The behavior can still be overridden by the environment variable,
    as before, only the default changes.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- test/integration_tests/config.py
+++ test/integration_tests/config.py
@@ -6,6 +6,6 @@ EXPECTED_TIME_TO_BOOT = 60  # seconds
 RESET = "\033[0m"
 BOLD = "\033[1m"
 RED = "\033[31m"
-OBJECTS = os.environ.get("OBJECTS", tempfile.mkdtemp(prefix="osbuild-"))
-OUTPUT_DIR = os.environ.get("OUTPUT_DIR", tempfile.mkdtemp(prefix="osbuild-"))
-OSBUILD = os.environ.get("OSBUILD", "osbuild").split(' ')
+OBJECTS = os.environ.get("OBJECTS", ".osbuild-test")
+OUTPUT_DIR = os.environ.get("OUTPUT_DIR", "output-test")
+OSBUILD = os.environ.get("OSBUILD", "python3 -m osbuild --libdir .").split(' ')
