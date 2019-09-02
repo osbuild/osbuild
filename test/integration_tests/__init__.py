3e380909ae69e710ff7aae90870afdf667ed2949
Martin Sehnoutka <msehnout@redhat.com>
2019-09-02 14:34:15 +0200

    fix: do not pass arguments to tests that does not take any

--- test/integration_tests/__init__.py
+++ test/integration_tests/__init__.py
@@ -3,7 +3,11 @@ from .config import *
 
 def evaluate_test(test, arg=None, name=None):
     try:
-        test(arg)
+        if arg:
+            test(arg)
+        else:
+            test()
+
         print(f"{RESET}{BOLD}{name or test.__name__}: Success{RESET}")
     except AssertionError as e:
         print(f"{RESET}{BOLD}{name or test.__name__}: {RESET}{RED}Fail{RESET}")
