osbuild/osbuild

commit 283281f04784fbc72656cb96c5ee9d847724035f

assemblers/tar: Allow creating tar archive without any compression
In tests we often use tar assembler as final stage. This means we
compress the image tree and decompress it right away. For this purposes
it is nice to have option to not have any compression. Actually,
this could very drastically improve CI running time.

A better option would be not to use tar at all and instead let osbuild
just dump the resulting tree. However, we felt this behaviour needs
more discussion and we need a fix asap.

assemblers/org.osbuild.tar
@@ -7,13 +7,18 @@ import sys
 
 def main(tree, output_dir, options):
     filename = options["filename"]
-    compression = options["compression"]
+    compression = options.get("compression")
 
-    if compression not in {"bzip2", "xz", "lzip", "lzma", "lzop", "gzip"}:
-        return 1
+    command = ["tar", "-cf", f"{output_dir}/{filename}", "-C", tree]
 
-    subprocess.run(["tar", f"--{compression}", "-cf", f"{output_dir}/{filename}", "-C", tree, "."],
-                   stdout=subprocess.DEVNULL, check=True)
+    if compression is not None:
+        if compression not in {"bzip2", "xz", "lzip", "lzma", "lzop", "gzip"}:
+            return 1
+        command.append(f"--{compression}")
+
+    command.append(".")
+
+    subprocess.run(command, stdout=subprocess.DEVNULL, check=True)
     return 0
 
 

commit 7e0264cecd2470de420ad9403f1a21a72702d9c7

assemblers/tar: fix compression
Commit 283281f broke compression by appending the argument last to the
tar command line. It needs to appear before the file.

Fix that and add a test.

[teg: add minor fix]

assemblers/org.osbuild.tar
@@ -40,16 +40,15 @@ def main(tree, output_dir, options):
     filename = options["filename"]
     compression = options.get("compression")
 
-    command = ["tar", "-cf", f"{output_dir}/{filename}", "-C", tree]
-
+    extra_args = []
     if compression is not None:
         if compression not in {"bzip2", "xz", "lzip", "lzma", "lzop", "gzip"}:
             return 1
-        command.append(f"--{compression}")
+        extra_args.append(f"--{compression}")
 
-    command.append(".")
+    subprocess.run(["tar", *extra_args, "-cf", f"{output_dir}/{filename}", "-C", tree, "."],
+                   stdout=subprocess.DEVNULL, check=True)
 
-    subprocess.run(command, stdout=subprocess.DEVNULL, check=True)
     return 0
 
 

test/test_assemblers.py
@@ -102,6 +102,22 @@ class TestAssemblers(osbuildtest.TestCase):
                                      1024 * 1024)
                     self.assertFilesystem(device + "p1", options["root_fs_uuid"], "ext4", tree_id)
 
+    def test_tar(self):
+        cases = [
+            ("tree.tar.gz", None, ["application/x-tar"]),
+            ("tree.tar.gz", "gzip", ["application/x-gzip", "application/gzip"])
+        ]
+        for filename, compression, expected_mimetypes in cases:
+            options = {
+                    "filename": filename,
+                    "compression": compression
+            }
+            tree_id, output_id = self.run_assembler("org.osbuild.tar", options)
+            image = f"{self.store}/refs/{output_id}/{filename}"
+            output = subprocess.check_output(["file", "--mime-type", image], encoding="utf-8")
+            _, mimetype = output.strip().split(": ") # "filename: mimetype"
+            self.assertIn(mimetype, expected_mimetypes)
+
 
 @contextlib.contextmanager
 def mount(device):