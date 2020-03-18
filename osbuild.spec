61420d149613a9aa221f241b7ff37ab8e365115d
Christian Kellner <christian@kellner.me>
2020-03-18 18:41:39 +0100

    10
    This completes osbuild version 10.

--- osbuild.spec
+++ osbuild.spec
@@ -1,6 +1,6 @@
 %global         forgeurl https://github.com/osbuild/osbuild
 
-Version:        9
+Version:        10
 
 %forgemeta
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="9",
+    version="10",
     description="A build system for OS images",
     packages=["osbuild"],
     license='Apache-2.0',
