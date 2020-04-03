75d9a0d6a1c35a4e6aacabfcc927c2c8bac5696f
Major Hayden <major@redhat.com>
2020-04-03 09:33:32 -0500

    Rework the boot test to exit sooner
    Signed-off-by: Major Hayden <major@redhat.com>

--- test/test_boot.py
+++ test/test_boot.py
@@ -1,4 +1,4 @@
-
+import os
 import subprocess
 import unittest
 from . import osbuildtest
@@ -6,24 +6,41 @@ from . import osbuildtest
 
 class TestBoot(osbuildtest.TestCase):
     def test_boot(self):
+        print("\n🏗️  Building Fedora 30 image for boot test.")
         _, output_id = self.run_osbuild("test/pipelines/f30-boot.json")
 
-        r = subprocess.run(["qemu-system-x86_64",
-            "-snapshot",
-            "-m", "1024",
-            "-M", "accel=kvm:hvf:tcg",
+        # Set up a boolean for test results.
+        boot_successful = False
 
-            # be silent
+        # Set up a command to boot the image via qemu.
+        qemu_boot_command = [
+            "qemu-system-x86_64",
             "-nographic",
-            "-monitor", "none",
-            "-serial", "none",
-
-            # create /dev/vport0p1
-            "-chardev", "stdio,id=stdio",
-            "-device", "virtio-serial",
-            "-device", "virtserialport,chardev=stdio",
-
+            "-serial", "mon:stdio",
             f"{self.get_path_to_store(output_id)}/f30-boot.qcow2"
-        ], encoding="utf-8", stdout=subprocess.PIPE, check=True)
+        ]
 
-        self.assertEqual(r.stdout.strip(), "running")
+        # Run qemu.
+        print("🤹‍♂️  Booting image via qemu.")
+        p = subprocess.Popen(
+            qemu_boot_command,
+            encoding="utf-8",
+            stdout=subprocess.PIPE
+        )
+        pid = p.pid
+
+        # Monitor the qemu output to know when the instance has booted.
+        print("🤔  Waiting for instance to fully boot.")
+        for line in iter(p.stdout.readline, ''):
+            if "Initializing machine ID from random generator" in line:
+                print("🥳  Instance has fully booted!")
+                boot_successful = True
+                break
+
+        # Stop qemu if it is still running.
+        os.kill(pid, 0)
+        p.kill()
+        p.wait()
+        p.terminate()
+
+        self.assertTrue(boot_successful)
