61b2d978aabfb4e0aa316bdf7789e7b0dd4e9d3d
Major Hayden <major@redhat.com>
2020-04-03 08:27:38 -0500

    Rework the boot test to exit sooner
    Signed-off-by: Major Hayden <major@redhat.com>

--- test/test_boot.py
+++ test/test_boot.py
@@ -6,9 +6,15 @@ from . import osbuildtest
 
 class TestBoot(osbuildtest.TestCase):
     def test_boot(self):
+        print("Building Fedora 30 image for boot test.")
         _, output_id = self.run_osbuild("test/pipelines/f30-boot.json")
 
-        r = subprocess.run(["qemu-system-x86_64",
+        # Set up a boolean for test results.
+        boot_successful = False
+
+        # Set up a command to boot the image via qemu.
+        qemu_boot_command = [
+            "qemu-system-x86_64",
             "-snapshot",
             "-m", "1024",
             "-accel", "kvm:hvf:tcg",
@@ -24,6 +30,27 @@ class TestBoot(osbuildtest.TestCase):
             "-device", "virtserialport,chardev=stdio",
 
             f"{self.get_path_to_store(output_id)}/f30-boot.qcow2"
-        ], encoding="utf-8", stdout=subprocess.PIPE, check=True)
+        ]
 
-        self.assertEqual(r.stdout.strip(), "running")
+        # Run qemu.
+        print("Booting image via qemu.")
+        r = subprocess.Popen(
+            qemu_boot_command,
+            encoding="utf-8",
+            stdout=subprocess.PIPE
+        )
+
+        # Monitor the qemu output for the "running" string which shows when
+        # the instance has fully booted.
+        print("Waiting for instance to fully boot.")
+        for line in iter(r.stdout.readline, ''):
+            # If the VM writes "running" to the console, everything worked!
+            if "running" in line:
+                print("Instance has fully booted!")
+                boot_successful = True
+                break
+
+        # Stop qemu if it is still running.
+        r.terminate()
+
+        self.assertTrue(boot_successful)
