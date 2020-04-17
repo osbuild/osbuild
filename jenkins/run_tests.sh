39e33e02e0cf6d1683c8b9264847a8cd161710fa
Major Hayden <major@redhat.com>
2020-04-17 12:00:30 -0500

    Add boot test to Jenkins
    Signed-off-by: Major Hayden <major@redhat.com>

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Ensure Ansible is installed.
-sudo dnf -y install ansible
+# Install packages.
+sudo dnf -y install ansible perl-XML-XPath
+
+get_fastest_mirror() {
+  FEDORA_VERSION=${1:-31}
+  curl -s "https://mirrors.fedoraproject.org/metalink?repo=fedora-${FEDORA_VERSION}&arch=x86_64" | \
+    xpath -e "/metalink/files/file/resources/url[@protocol='http']/text()" 2>/dev/null | \
+    head -n 1 | \
+    sed 's#releases.*##' || true
+}
 
 # Clone the latest version of ansible-osbuild.
 git clone https://github.com/osbuild/ansible-osbuild.git ansible-osbuild
@@ -19,3 +27,11 @@ pushd ansible-osbuild
     -e osbuild_version=${OSBUILD_VERSION} \
     playbook.yml
 popd
+
+# Modify the URLs in the test cases to use a nearby mirror.
+TEST_CASE_DIR=/usr/share/tests/osbuild-composer
+FASTEST_MIRROR=$(get_fastest_mirror)
+sed -i "s#http://download.fedoraproject.org/pub/fedora/linux/releases/#${FASTEST_MIRROR}releases/#" test/pipelines/*.json
+
+# Run a boot test
+python3 -m unittest -v test.test_boot
