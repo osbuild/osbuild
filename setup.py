07d62dc41a823e2200f197656a2f305873a16827
David Rheinsberg <david.rheinsberg@gmail.com>
2020-04-21 16:29:19 +0200

    util: mark as module
    Make sure ./osbuild/util/ is considered a python module. Add
    __init__.py to declare it as such and include it in the module-list of
    setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="osbuild",
     version="12",
     description="A build system for OS images",
-    packages=["osbuild"],
+    packages=["osbuild", "osbuild.util"],
     license='Apache-2.0',
     entry_points={
         "console_scripts": ["osbuild = osbuild.__main__:main"]
