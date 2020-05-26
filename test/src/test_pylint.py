39e989245d997db1623249d37534e3e07fc0962e
David Rheinsberg <david.rheinsberg@gmail.com>
2020-05-26 17:37:19 +0200

    test: enable pylint warnings
    Make pylint complain about warnings, not just errors. There are lots of
    useful warnings and we generally do adhere to the coding-styles.

--- test/src/test_pylint.py
+++ test/src/test_pylint.py
@@ -18,11 +18,10 @@ class TestPylint(test.TestBase, unittest.TestCase):
         #
 
         path = self.locate_test_checkout()
-        options = "--errors-only"
 
-        subprocess.run(f"find {path} -type f -name '*.py' | xargs pylint {options}",
+        subprocess.run(f"find {path} -type f -name '*.py' | xargs pylint",
                        shell=True, check=True)
-        subprocess.run(f"pylint {options}"
+        subprocess.run("pylint"
                        + f" {path}/assemblers/*"
                        + f" {path}/runners/*"
                        + f" {path}/sources/*"
