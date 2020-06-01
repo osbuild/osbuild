26d2c6356b067c4e1da1c16703257cb6f7f076d7
Major Hayden <major@redhat.com>
2020-06-01 07:50:48 -0500

    CI: Store RPMs by SHA
    This commit brings over updates for mock builds from osbuild-composer.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- schutzbot/run_tests.sh
+++ schutzbot/run_tests.sh
@@ -5,18 +5,8 @@ set -euxo pipefail
 source /etc/os-release
 
 # Set up a dnf repository for the RPMs we built via mock.
-sudo tee /etc/yum.repos.d/osbuild-mock.repo > /dev/null << EOF
-[osbuild-mock]
-name=osbuild mock ${BUILD_TAG} ${ID}${VERSION_ID//./}
-baseurl=${MOCK_REPO_BASE_URL}/${BUILD_TAG}/${ID}${VERSION_ID//./}
-enabled=1
-gpgcheck=0
-# Default dnf repo priority is 99. Lower number means higher priority.
-priority=5
-EOF
-
-# Verify that the repository we added is working properly.
-sudo dnf list all | grep osbuild-mock
+sudo cp osbuild-mock.repo /etc/yum.repos.d/osbuild-mock.repo
+dnf repository-packages osbuild-mock list
 
 # Create temporary directories for Ansible.
 sudo mkdir -vp /opt/ansible_{local,remote}
@@ -37,9 +27,6 @@ trap "preserve_journal" ERR
 
 # Write a simple hosts file for Ansible.
 echo -e "[test_instances]\nlocalhost ansible_connection=local" > hosts.ini
-
-# Get the SHA of osbuild which Jenkins checked out for us.
-OSBUILD_VERSION=$(git rev-parse HEAD)
 
 # Deploy osbuild-composer and osbuild using RPMs built in a mock chroot.
 # NOTE(mhayden): Jenkins clones the repository and then merges the code from
