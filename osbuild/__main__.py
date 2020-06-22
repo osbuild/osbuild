ff850e24f8aaef502a3cf0cc8bff4ad40518d021
Christian Kellner <christian@kellner.me>
2020-06-22 17:40:35 +0200

    osbuild: use osbuild_cli directly as main entry
    Instead of having a another indirection via `main_cli`, directly
    use `osbuild_cli` in as main function in `__main__.py`. Also use
    that in as the entry point for the generated `osbuild` executable.
    Change `osbuild_cli` to be self-contained, i.e. it directly uses
    `sys.argv` and `sys.exit`.

--- osbuild/__main__.py
+++ osbuild/__main__.py
@@ -4,10 +4,9 @@ This specifies the entrypoint of the osbuild module when run as executable. For
 compatibility we will continue to run the CLI.
 """
 
-import sys
 
-from osbuild.main_cli import main_cli as main
+from osbuild.main_cli import osbuild_cli as main
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
