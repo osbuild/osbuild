7f249afe07d6ae0cda08fbf439359ec99e78dd37
Christian Kellner <christian@kellner.me>
2020-09-10 12:34:35 +0200

    20
    This completes the development of osbuild version 20.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="20",
+    version="21",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.util"],
     license='Apache-2.0',
