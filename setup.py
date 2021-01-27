6a4be1a6999543fc1af32bcf398ec56556fc784f
Christian Kellner <christian@kellner.me>
2021-01-27 17:30:25 +0000

    24
    This completes the development of osbuild version 24

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="23",
+    version="24",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
