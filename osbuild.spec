755c07a142ce2e8e88420590be553d8a255f3326
Christian Kellner <christian@kellner.me>
2021-02-12 19:30:07 +0100

    25
    This completes the development of osbuild version 25

--- osbuild.spec
+++ osbuild.spec
@@ -1,7 +1,7 @@
 %global         forgeurl https://github.com/osbuild/osbuild
 %global         selinuxtype targeted
 
-Version:        24
+Version:        25
 
 %forgemeta
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="24",
+    version="25",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
