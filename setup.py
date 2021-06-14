bc7096ab8694a1a0abcf7bf536abee8ea8a6e5ac
Christian Kellner <christian@kellner.me>
2021-06-14 15:06:06 +0000

    29
    This completes the development of osbuild version 29.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="28",
+    version="29",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
