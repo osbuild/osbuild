8fc92927428e4a4facb7022d144517be80adcbcc
Achilleas Koutsou <achilleas@koutsou.net>
2021-06-14 17:37:05 +0200

    util/path: add path checker helper function
    Checks if one path is a child of a second one. Useful for checking if
    paths defined in a manifest exist inside the tree.
    Optionally checks if the target path exists.

---
+++ osbuild/util/path.py
@@ -0,0 +1,14 @@
+"""Path handling utility functions"""
+import os.path
+
+from .types import PathLike
+
+
+def in_tree(path: PathLike, tree: PathLike, must_exist=False) -> bool:
+    """Return whether the canonical location of 'path' is under 'tree'.
+    If 'must_exist' is True, the file must also exist for the check to succeed.
+    """
+    path = os.path.abspath(path)
+    if path.startswith(tree):
+        return not must_exist or os.path.exists(path)
+    return False
