ee46f89647786ff1b64e96be10670facd5cba76a
jkozol <jkozol@users.noreply.github.com>
2022-02-02 17:03:08 +0000

    Post release version bump
    [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="47",
+    version="48",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
