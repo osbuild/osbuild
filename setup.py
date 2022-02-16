5a7d3eee86e1bb2f018dc9183b864cddd6a5cb21
chloenayon <chloenayon@users.noreply.github.com>
2022-02-16 14:16:11 +0000

    Post release version bump
    [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="48",
+    version="49",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
