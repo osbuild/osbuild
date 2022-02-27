20231c8918876615f50b3c18d6066cb11d5a91e0
ochosi <ochosi@users.noreply.github.com>
2022-02-27 19:00:46 +0000

    Post release version bump
    [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="50",
+    version="51",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
