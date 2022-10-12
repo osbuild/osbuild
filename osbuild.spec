6523fd8d4139d35cf365ef1affeb081e6a37d304
schutzbot <schutzbot@users.noreply.github.com>
2022-10-12 08:19:37 +0000

    Post release version bump
    [skip ci]

--- osbuild.spec
+++ osbuild.spec
@@ -1,7 +1,7 @@
 %global         forgeurl https://github.com/osbuild/osbuild
 %global         selinuxtype targeted
 
-Version:        69
+Version:        70
 
 %forgemeta
 
--- osbuild/__init__.py
+++ osbuild/__init__.py
@@ -10,7 +10,7 @@ independent of osbuild but used across the osbuild codebase.
 
 from .pipeline import Manifest, Pipeline, Stage
 
-__version__ = "69"
+__version__ = "70"
 
 __all__ = [
     "Manifest",
