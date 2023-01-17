2870f92f59cf8efd8a463fd5784b4c85fc08757b
Tomáš Hozza <thozza@redhat.com>
2023-01-17 18:12:03 +0100

    schutzbot/deploy.sh: don't install `osbuild-composer-tests`
    osbuild CI tests, specifically image tests are not executing the test
    script from osbuild-composer any more, but it uses manifest-db.
    
    Therefore it is not needed to set up repository for osbuild-composer nor
    install it.
    
    Delete all unneeded lines from the script and replace them by installing
    all osbuild packages needed to build images from `manifest-db`.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- schutzbot/deploy.sh
+++ schutzbot/deploy.sh
@@ -3,10 +3,6 @@ set -euxo pipefail
 
 DNF_REPO_BASEURL=http://osbuild-composer-repos.s3.amazonaws.com
 
-# The osbuild-composer commit to run reverse-dependency test against.
-# Currently: CI: temporarily switch RHOS-01 to non-ssd instances
-OSBUILD_COMPOSER_COMMIT=de72b36dddfc703d76d79479dde7b92f0a78e924
-
 # Get OS details.
 source /etc/os-release
 ARCH=$(uname -m)
@@ -32,27 +28,12 @@ enabled=1
 gpgcheck=0
 # Default dnf repo priority is 99. Lower number means higher priority.
 priority=5
-
-[osbuild-composer]
-name=osbuild-composer ${OSBUILD_COMPOSER_COMMIT}
-baseurl=${DNF_REPO_BASEURL}/osbuild-composer/${DISTRO_VERSION}/${ARCH}/${OSBUILD_COMPOSER_COMMIT}
-enabled=1
-gpgcheck=0
-# Give this a slightly lower priority, because we used to have osbuild in this repo as well.
-priority=10
 EOF
 
-if [[ $ID == rhel || $ID == centos ]] && ! rpm -q epel-release; then
-    # Set up EPEL repository (for ansible and koji)
-    sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID%.*}.noarch.rpm
-fi
-
-# Install the Image Builder packages.
-# Note: installing only -tests to catch missing dependencies
-sudo dnf -y install osbuild-composer-tests
-
-# Set up a directory to hold repository overrides.
-sudo mkdir -p /etc/osbuild-composer/repositories
-
-# Temp fix until composer gains these dependencies
-sudo dnf -y install osbuild-luks2 osbuild-lvm2
+# install pckages needed to run tests
+sudo dnf install -y osbuild \
+                    osbuild-ostree \
+                    osbuild-lvm2 \
+                    osbuild-luks2 \
+                    jq \
+                    python3
