a7b3ef393657f7078a147bcaa405508f39107000
schutzbot <schutzbot@users.noreply.github.com>
2023-05-24 08:13:14 +0000

    Post release version bump
    [skip ci]

--- osbuild.spec
+++ osbuild.spec
@@ -1,7 +1,7 @@
 %global         forgeurl https://github.com/osbuild/osbuild
 %global         selinuxtype targeted
 
-Version:        86
+Version:        87
 
 %forgemeta
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="86",
+    version="87",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
