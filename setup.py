21180b567c7e39278cb77815600cfccd01fff362
Simon de Vlieger <cmdr@supakeen.com>
2023-08-02 16:33:37 +0200

    lint: setup.py fixes
    `setup.py` will be added to lintables, fix it before it fails in CI.

--- setup.py
+++ setup.py
@@ -5,17 +5,10 @@ setuptools.setup(
     version="92",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
-    license='Apache-2.0',
+    license="Apache-2.0",
     install_requires=[
         "jsonschema",
     ],
-    entry_points={
-        "console_scripts": [
-            "osbuild = osbuild.main_cli:osbuild_cli"
-        ]
-    },
-    scripts=[
-        "tools/osbuild-mpp",
-        "tools/osbuild-dev"
-    ],
+    entry_points={"console_scripts": ["osbuild = osbuild.main_cli:osbuild_cli"]},
+    scripts=["tools/osbuild-mpp", "tools/osbuild-dev"],
 )
