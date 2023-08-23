271f5eff31c404b8a43ee8015f03fe6394981c21
schutzbot <schutzbot@users.noreply.github.com>
2023-08-23 15:27:18 +0000

    Post release version bump
    [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="93",
+    version="94",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
