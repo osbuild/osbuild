4e2f17bfcacdfce3782c03b0e3442fbfba7982b4
schutzbot <schutzbot@users.noreply.github.com>
2023-09-13 08:16:15 +0000

    Post release version bump
    [skip ci]

--- osbuild/__init__.py
+++ osbuild/__init__.py
@@ -10,7 +10,7 @@ independent of osbuild but used across the osbuild codebase.
 
 from .pipeline import Manifest, Pipeline, Stage
 
-__version__ = "95"
+__version__ = "96"
 
 __all__ = [
     "Manifest",
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="95",
+    version="96",
     description="A build system for OS images",
     packages=["osbuild", "osbuild.formats", "osbuild.util"],
     license='Apache-2.0',
