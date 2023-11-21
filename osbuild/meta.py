02490809f4bc7eeb417bca76e96438de94a002a0
Michael Vogt <michael.vogt@gmail.com>
2023-11-21 16:49:32 +0100

    tox,osbuild: deal with stages that have external json data
    Some existing code/test assumes that anything in `stages/` is
    executable. This commit fixes this and excludes e.g. json from
    lintables.

--- osbuild/meta.py
+++ osbuild/meta.py
@@ -466,6 +466,8 @@ class ModuleInfo:
 
         docstring = ast.get_docstring(tree)
         doclist = docstring.split("\n") if docstring else []
+        summary = doclist[0] if len(doclist) > 0 else ""
+        long_description = "\n".join(doclist[1:]) if len(doclist) > 0 else ""
 
         assigns = filter_type(tree.body, ast.Assign)
         values = {
@@ -486,8 +488,8 @@ class ModuleInfo:
                 "1": parse_schema(values.get("SCHEMA")),
                 "2": parse_schema(values.get("SCHEMA_2")),
             },
-            'desc': doclist[0],
-            'info': "\n".join(doclist[1:]),
+            'desc': summary,
+            'info': long_description,
             'caps': parse_caps(values.get("CAPABILITIES"))
         }
         return cls(klass, name, path, info)
@@ -617,7 +619,7 @@ class Index:
             raise ValueError(f"Unsupported nodule class: {klass}")
 
         path = os.path.join(self.path, module_path)
-        modules = filter(lambda f: os.path.isfile(f"{path}/{f}"),
+        modules = filter(lambda f: os.path.isfile(f"{path}/{f}") and not path.endswith("-meta.json"),
                          os.listdir(path))
         return list(modules)
 
