eea81b660a12b562e5d244e714c97402c7e17106
Alexander Larsson <alexl@redhat.com>
2023-12-08 09:21:36 +0100

    testutils: Add assert_dict_has
    This is a convenient way for tests to assert that some nested dicts
    (like a parsed json) has a particular key/value somewhere in it.
    
    For example:
      assert_dict_has(config, "toplevel.subitem.key", True)

--- osbuild/testutil/__init__.py
+++ osbuild/testutil/__init__.py
@@ -6,3 +6,10 @@ import shutil
 
 def has_executable(executable: str) -> bool:
     return shutil.which(executable) is not None
+
+
+def assert_dict_has(v, keys, expected_value):
+    for key in keys.split("."):
+        assert key in v
+        v = v[key]
+    assert v == expected_value
