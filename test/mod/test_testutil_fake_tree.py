d58c7550cea4904e3a3a99ac8ddefaecf95fa70a
Michael Vogt <michael.vogt@gmail.com>
2023-12-15 20:32:33 +0100

    testutil: extrace new `make_fake_tree()` helper
    Extract a new helper `make_fake_tree()` that generalizes the existing
    helper `make_fake_input_tree()`. The later will always create the
    content under `{basedir}/tree` which is convinient for input tree
    based tests but too specialized when using it in different contexts.
    
    The existing `make_fake_input_tree()` is preserved unchanged and
    becomes just a tiny wrapper.

--- test/mod/test_testutil_fake_tree.py
+++ test/mod/test_testutil_fake_tree.py
@@ -3,16 +3,30 @@
 #
 import os.path
 
-from osbuild.testutil import make_fake_input_tree
+from osbuild.testutil import make_fake_input_tree, make_fake_tree
+
+TEST_INPUT_TREE = {
+    "/fake-file-one": "Some content",
+    "/second/fake-file-two": "Second content",
+}
 
 
-def test_make_fake_tree(tmp_path):  # pylint: disable=unused-argument
-    fake_input_tree = make_fake_input_tree(tmp_path, {
-        "/fake-file-one": "Some content",
-        "/second/fake-file-two": "Second content",
-    })
+def validate_test_input_tree(fake_input_tree):
     assert os.path.isdir(fake_input_tree)
     assert os.path.exists(os.path.join(fake_input_tree, "fake-file-one"))
     assert open(os.path.join(fake_input_tree, "fake-file-one"), encoding="utf8").read() == "Some content"
     assert os.path.exists(os.path.join(fake_input_tree, "second/fake-file-two"))
-    assert open(os.path.join(fake_input_tree, "second/fake-file-two"), encoding="utf").read() == "Second content"
+    assert open(os.path.join(fake_input_tree, "second/fake-file-two"), encoding="utf8").read() == "Second content"
+
+
+def test_make_fake_tree(tmp_path):
+    make_fake_tree(tmp_path, TEST_INPUT_TREE)
+    validate_test_input_tree(tmp_path)
+
+
+def test_make_fake_input_tree(tmp_path):
+    # make_fake_input_tree is a convinience wrapper around make_fake_tree
+    # for the osbuild input trees
+    fake_input_tree = make_fake_input_tree(tmp_path, TEST_INPUT_TREE)
+    assert fake_input_tree.endswith("/tree")
+    validate_test_input_tree(fake_input_tree)
