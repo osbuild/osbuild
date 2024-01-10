106681f41e8359e7b2bd1f0e091e48ec32f3b61d
Luke Yang <luyang@redhat.com>
2024-01-10 10:49:45 -0500

    stages/org.osbuild.mkfs.ext4: add ext4 options
    Add optional flags to the org.osbuild.mkfs.ext4 stage enabling/disabling
    the metadata_csum_seed and orphan_file features.

--- stages/test/test_mkfs_ext4.py
+++ stages/test/test_mkfs_ext4.py
@@ -82,6 +82,11 @@ def test_mkfs_ext4_integration(tmp_path):
     ({}, []),
     ({"verity": True}, ["-O", "verity"]),
     ({"verity": False}, ["-O", "^verity"]),
+    ({"orphan_file": True}, ["-O", "orphan_file"]),
+    ({"orphan_file": False}, ["-O", "^orphan_file"]),
+    ({"metadata_csum_seed": True}, ["-O", "metadata_csum_seed"]),
+    ({"metadata_csum_seed": False}, ["-O", "^metadata_csum_seed"]),
+    ({"verity": True, "orphan_file": True, "metadata_csum_seed": True}, ["-O", "verity", "-O", "orphan_file", "-O", "metadata_csum_seed"]),
 ])
 @mock.patch("subprocess.run")
 def test_mkfs_ext4_cmdline(mock_run, test_input, expected):
