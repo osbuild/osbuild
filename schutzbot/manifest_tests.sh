3b7a8aa728e2a98c65ae8921417bc2cda205114c
Tom Koscielniak <tkosciel@redhat.com>
2024-03-04 10:38:37 +0100

    Migrate manifest tests to new RHEL GA version

--- schutzbot/manifest_tests.sh
+++ schutzbot/manifest_tests.sh
@@ -3,8 +3,7 @@ set -euxo pipefail
 # Get OS details.
 source /etc/os-release
 ARCH=$(uname -m)
-DISTRO_CODE="${DISTRO_CODE:-${ID}-${VERSION_ID//./}}"
-
+DISTRO_CODE="${DISTRO_CODE:-${ID}-${VERSION_ID}}"
 # get manifest-db at the version specified for this arch+distro
 MANIFEST_DB_COMMIT=$(cat Schutzfile | jq -r '.global.dependencies."manifest-db".commit')
 MANIFEST_DB_REPO="https://github.com/osbuild/manifest-db"
