9393211b8acf94ee83d8300549f8bd4ada5a595f
Michael Vogt <michael.vogt@gmail.com>
2024-03-11 11:21:11 +0100

    testutil: tweak `mock_command` to write a call_log
    Instead of just mocking the binary also write a log of the way
    it got called so that tests can use this to check if the right
    options are passed.
    
    Note that the API should be improved here, instead of returning
    a "naked" path to the calllog file there should be a class wrapping
    it. And of course there should be tests.

--- test/mod/test_testutil_mock_command.py
+++ test/mod/test_testutil_mock_command.py
@@ -12,14 +12,26 @@ def test_mock_command_integration():
     output = subprocess.check_output(["echo", "hello"])
     assert output == b"hello\n"
     fake_echo = textwrap.dedent("""\
-    #!/bin/sh
     echo i-am-not-echo
     """)
-    with mock_command("echo", fake_echo):
+    with mock_command("echo", fake_echo) as mocked_cmd:
         output = subprocess.check_output(["echo", "hello"])
         assert output == b"i-am-not-echo\n"
+        assert mocked_cmd.call_args_list == [
+            ["hello"],
+        ]
     output = subprocess.check_output(["echo", "hello"])
     assert output == b"hello\n"
+
+
+def test_mock_command_multi():
+    with mock_command("echo", "") as mocked_cmd:
+        subprocess.check_output(["echo", "call1-arg1", "call1-arg2"])
+        subprocess.check_output(["echo", "call2-arg1", "call2-arg2"])
+        assert mocked_cmd.call_args_list == [
+            ["call1-arg1", "call1-arg2"],
+            ["call2-arg1", "call2-arg2"],
+        ]
 
 
 def test_mock_command_environ_is_modified_and_restored():
