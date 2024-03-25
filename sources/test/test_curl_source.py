25bb784e4cf37024d0d9d858a0218b4db689f33f
Michael Vogt <michael.vogt@gmail.com>
2024-03-25 09:42:34 +0100

    sources: add new `sources_service` fixture
    Similar to the previous commit to include a `inputs_service` fixture
    this does the same for `source.SourcesService` imports.
    
    Note that we cannot easily share the helpers so we have to life with
    a bit of very similar but duplicated code. To fix this we would have
    to have a shared confftest.py that pytest can find. Which would mean
    that we need to put the tests under a common dir that is reachable
    via __init__.py files (which we currently not have because stages,
    inputs etc do not have a __init__.py so python does not considers
    them modules).

--- sources/test/test_curl_source.py
+++ sources/test/test_curl_source.py
@@ -7,41 +7,33 @@ import tempfile
 
 import pytest
 
-from osbuild import testutil
-
 SOURCES_NAME = "org.osbuild.curl"
 
 
-def test_curl_source_not_exists(sources_module):
-    fd = testutil.make_fake_service_fd()
-    curl_source = sources_module.CurlSource.from_args(["--service-fd", str(fd)])
+def test_curl_source_not_exists(sources_service):
     tmpdir = tempfile.TemporaryDirectory()
-    curl_source.cache = tmpdir.name
+    sources_service.cache = tmpdir.name
     desc = {
         "url": "http://localhost:80/a",
     }
     checksum = "sha256:1234567890123456789012345678901234567890909b14ffb032aa20fa23d9ad6"
-    assert not curl_source.exists(checksum, desc)
+    assert not sources_service.exists(checksum, desc)
 
 
-def test_curl_source_exists(sources_module):
-    fd = testutil.make_fake_service_fd()
-    curl_source = sources_module.CurlSource.from_args(["--service-fd", str(fd)])
+def test_curl_source_exists(sources_service):
     tmpdir = tempfile.TemporaryDirectory()
-    curl_source.cache = tmpdir.name
+    sources_service.cache = tmpdir.name
     desc = {
         "url": "http://localhost:80/a",
     }
     checksum = "sha256:1234567890123456789012345678901234567890909b14ffb032aa20fa23d9ad6"
     pathlib.Path(os.path.join(tmpdir.name, checksum)).touch()
-    assert curl_source.exists(checksum, desc)
+    assert sources_service.exists(checksum, desc)
 
 
-def test_curl_source_amend_secrets(sources_module):
-    fd = testutil.make_fake_service_fd()
-    curl_source = sources_module.CurlSource.from_args(["--service-fd", str(fd)])
+def test_curl_source_amend_secrets(sources_service):
     tmpdir = tempfile.TemporaryDirectory()
-    curl_source.cache = tmpdir.name
+    sources_service.cache = tmpdir.name
     desc = {
         "url": "http://localhost:80/a",
         "secrets": {
@@ -59,17 +51,15 @@ def test_curl_source_amend_secrets(sources_module):
         cm.callback(cb)
         checksum = "sha256:1234567890123456789012345678901234567890909b14ffb032aa20fa23d9ad6"
         pathlib.Path(os.path.join(tmpdir.name, checksum)).touch()
-        new_desc = curl_source.amend_secrets(checksum, desc)
+        new_desc = sources_service.amend_secrets(checksum, desc)
         assert new_desc[1]["secrets"]["ssl_client_key"] == "key"
         assert new_desc[1]["secrets"]["ssl_client_cert"] == "cert"
         assert new_desc[1]["secrets"]["ssl_ca_cert"] is None
 
 
-def test_curl_source_amend_secrets_fail(sources_module):
-    fd = testutil.make_fake_service_fd()
-    curl_source = sources_module.CurlSource.from_args(["--service-fd", str(fd)])
+def test_curl_source_amend_secrets_fail(sources_service):
     tmpdir = tempfile.TemporaryDirectory()
-    curl_source.cache = tmpdir.name
+    sources_service.cache = tmpdir.name
     desc = {
         "url": "http://localhost:80/a",
         "secrets": {
@@ -79,5 +69,5 @@ def test_curl_source_amend_secrets_fail(sources_module):
     checksum = "sha256:1234567890123456789012345678901234567890909b14ffb032aa20fa23d9ad6"
     pathlib.Path(os.path.join(tmpdir.name, checksum)).touch()
     with pytest.raises(RuntimeError) as exc:
-        curl_source.amend_secrets(checksum, desc)
+        sources_service.amend_secrets(checksum, desc)
     assert "mtls secrets required" in str(exc)
