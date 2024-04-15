5bd16406e7c8ca4d94269ed3d7ac50143367f67e
Michael Vogt <michael.vogt@gmail.com>
2024-04-15 09:51:27 +0200

    stages(groups): port to v2 and allow devices/mounts
    This will allow us to generate `bootc install to-filesystem` compatible
    devices/mount setups for the groups stage.

--- stages/test/test_groups.py
+++ stages/test/test_groups.py
@@ -29,3 +29,10 @@ def test_schema_validation(stage_schema, test_data, expected_err):
     else:
         assert res.valid is False
         assert_jsonschema_error_contains(res, expected_err, expected_num_errs=1)
+
+
+def test_schema_supports_bootc_style_mounts(stage_schema, bootc_devices_mounts_dict):
+    test_input = bootc_devices_mounts_dict
+    test_input["type"] = STAGE_NAME
+    res = stage_schema.validate(test_input)
+    assert res.valid is True, f"err: {[e.as_dict() for e in res.errors]}"
