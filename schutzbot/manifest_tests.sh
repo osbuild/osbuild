cbd6693a35d70760953ca9f2e4427c9a7a87e1eb
Michael Vogt <michael.vogt@gmail.com>
2024-05-02 10:39:04 +0200

    fixup! include patch to manifest-db to get visiblity on gitlab failures

--- schutzbot/manifest_tests.sh
+++ schutzbot/manifest_tests.sh
@@ -22,4 +22,22 @@ IFS='/' read -r -a array <<< $1
 
 # run the tests from the manifest-db for this arch+distro
 echo "Running the osbuild-image-test for arch $ARCH and ditribution $DISTRO_CODE"
+
+# tweak for more debug visiblity (should go upstream)
+sudo dnf install -y patch
+cat <<EOF | patch -p1
+diff --git a/tools/osbuild-image-test b/tools/osbuild-image-test
+index aee8269..f4ed100 100755
+--- a/tools/osbuild-image-test
++++ b/tools/osbuild-image-test
+@@ -60,6 +60,8 @@ class OSBuild:
+             "--store", os.fspath(self.store),
+             "--output-dir", os.fspath(self.outdir),
+             "--json",
++            # improve visibility for real errors by logging to stderr
++            "--monitor=LogMonitor", "--monitor-fd=2",
+         ]
+ 
+         if args:
+EOF
 sudo tools/osbuild-image-test --arch=$ARCH --distro=$DISTRO_CODE --image-info-path=tools/image-info --instance-number="${array[0]}" --total-number-of-instances="${array[1]}"
