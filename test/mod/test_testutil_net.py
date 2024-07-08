be6933999f63a22ea312d034077d1ebffa27325a
Michael Vogt <michael.vogt@gmail.com>
2024-07-08 21:34:12 +0200

    fixup! add test_http_ssl_serve_directory_multi_cert() curl test

--- test/mod/test_testutil_net.py
+++ test/mod/test_testutil_net.py
@@ -37,3 +37,40 @@ def test_http_ssl_serve_directory_smoke(tmp_path):
              f"https://localhost:{httpd.server_port}/file1"],
         )
         assert output == b"file1 content"
+
+
+def test_http_ssl_serve_directory_multi_cert(tmp_path):
+    httpd_dir1 = tmp_path / "httpd1"
+    make_fake_tree(httpd_dir1, {
+        "file1": "file1 content",
+    })
+    httpd_dir2 = tmp_path / "httpd2"
+    make_fake_tree(httpd_dir2, {
+        "file2": "file2 content",
+    })
+
+    cert_dir = pathlib.Path(__file__).parent.parent / "data/certs"
+    cacertfile1 = cert_dir / "cert1.pem"
+    keyfile1 = cert_dir / "key1.pem"
+    cacertfile2 = cert_dir / "cert2.pem"
+    keyfile2 = cert_dir / "key2.pem"
+
+    with https_serve_directory(httpd_dir1, cacertfile1, keyfile1) as httpd1:
+        with https_serve_directory(httpd_dir2, cacertfile2, keyfile2) as httpd2:
+            curl_config = tmp_path / "config.txt"
+            # note that the "next" in there is critcial
+            curl_config.write_text(textwrap.dedent(f"""\
+            url = "https://localhost:{httpd1.server_port}/file1"
+            cacert = {cacertfile1}
+            next
+
+            url = "https://localhost:{httpd2.server_port}/file2"
+            cacert = {cacertfile2}
+            """))
+            print(curl_config.read_text())
+            output = subprocess.check_output(
+                ["curl",
+                 "--config", os.fspath(curl_config),
+                ],
+            )
+        assert output == b"file1 contentfile2 content"
