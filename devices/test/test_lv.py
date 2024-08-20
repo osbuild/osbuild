5dc7930c68a060be33f0f11e5480955a1fe9fb1e
Michael Vogt <michael.vogt@gmail.com>
2024-08-20 18:17:15 +0200

    devices: create `/dev/mapper/<logical-volume>` as well
    When we create the device node inside the buildroot so far it's
    very minimal - just `/dev/{vg}-{lv}` with the appopriate major/minor.
    
    However when mount runs it will create a mapper device with the
    same major/minor under `/dev/mapper/{escaped(vg)}-{escaped(lv)}`
    and use that to mount the actual filesystem. Without this additional
    device findmnt will not be able to detect the udev attributes of
    the source (as the source is just missing from /dev).
    
    This commit create the right mapper in the same way that we
    create the non-mapper device node.

--- devices/test/test_lv.py
+++ devices/test/test_lv.py
@@ -10,3 +10,8 @@ DEVICES_NAME = "org.osbuild.lvm2.lv"
 def test_lvm2_lv_get_parent_path(devices_module, parent, options, expected_parent_path):
     pp = devices_module.get_parent_path(parent, options)
     assert pp == expected_parent_path
+
+
+def test_lvm2_escaped_lv_mapper_name(devices_module):
+    expected = "1d2b2150--8de2--4b68--b387--f9bc709190e8-lvroot"
+    assert devices_module.escaped_lv_mapper_name("1d2b2150-8de2-4b68-b387-f9bc709190e8", "lvroot") == expected
