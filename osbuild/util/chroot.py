be8618416ae80ba841b244bac47694dfd390b362
Achilleas Koutsou <achilleas@koutsou.net>
2024-08-21 12:29:50 +0200

    osbuild/util: new module: chroot
    New chroot utility module that sets up a tree with the necessary virtual
    filesystems needed for running commands in the root tree in a similar
    environment as they would run in the build root.
    
    This is needed for some stages, but may also be used for all chroot
    calls to unify the setup and teardown of the root environment.
    
    The Chroot context class was previously part of the org.osbuild.dracut
    stage, which was the first stage to need this setup.

---
+++ osbuild/util/chroot.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+
+class Chroot:
+    """
+    Sets up mounts for the virtual filesystems inside a root tree, preparing it for running commands using chroot. This
+    should be used whenever a stage needs to run a command against the root tree but doesn't support a --root option or
+    similar.
+    Cleans up mounts when done.
+
+    This mounts /proc, /dev, and /sys.
+    """
+
+    def __init__(self, root: str):
+        self.root = root
+
+    def __enter__(self):
+        for d in ["/proc", "/dev", "/sys"]:
+            if not os.path.exists(self.root + d):
+                print(f"Making missing chroot directory: {d}")
+                os.makedirs(self.root + d)
+
+        subprocess.check_call(["/usr/bin/mount",
+                               "-t", "proc",
+                               "-o", "nosuid,noexec,nodev",
+                               "proc", f"{self.root}/proc"])
+        subprocess.check_call(["/usr/bin/mount",
+                               "-t", "devtmpfs",
+                               "-o", "mode=0755,noexec,nosuid,strictatime",
+                               "devtmpfs", f"{self.root}/dev"])
+        subprocess.check_call(["/usr/bin/mount",
+                               "-t", "sysfs",
+                               "-o", "nosuid,noexec,nodev",
+                               "sysfs", f"{self.root}/sys"])
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, tracebk):
+        for d in ["/proc", "/dev", "/sys"]:
+            subprocess.check_call(["/usr/bin/umount", self.root + d])
