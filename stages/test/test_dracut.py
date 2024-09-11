a3e32f382386409d688b5cf4e8126569292c89b0
Michael Vogt <michael.vogt@gmail.com>
2024-09-11 09:50:41 +0200

    util: drop absolute path from `Chroot.run()` calls
    We currently use the absolute path of these binaries in the
    helper. This has some advantages but given that we control the
    inputs for PATH in general it seems unnecessary.
    
    We are also slightly inconsistent about this in the codebase but
    favor the non absolute path version. A quick count:
    ```
    $ git grep '"chroot"'|wc -l
    13
    $ git grep '"/usr/sbin/chroot"'|grep -v test_|wc -l
    8
    ```
    for `mount` and `umount` it seems this is the only place that uses
    the absolute path.
    
    It's not an important change but it has the nice property that it
    allows us to use e.g. `testutil.mock_command()` in our tests and
    it would be nice to be consistent.

--- stages/test/test_dracut.py
+++ stages/test/test_dracut.py
@@ -27,5 +27,5 @@ def test_dracut_with_initoverlayfs(mocked_run, tmp_path, stage_module, with_init
     args, kwargs = mocked_run.call_args_list[3]  # chroot is the 4th call
     assert kwargs.get("check") is True
     run_argv = args[0]
-    assert run_argv[0] == "/usr/sbin/chroot"
+    assert run_argv[0] == "chroot"
     assert run_argv[2] == expected_argv2
