78cc4ea53ccb99d0b1fb120aff751d60210af593
Brian C. Lane <bcl@redhat.com>
2024-11-21 13:13:56 -0800

    grub2.inst: Add support for creating iso9660 boot image
    Don't include the "location" offset, and use just a path for the prefix
    section to set the path of the grub2 modules on the iso. eg.
    
    {
        "filename": "eltorito.img",
        "platform": "i386-pc",
        "core": {
            "type": "mkimage",
            "partlabel": "gpt",
            "filesystem": "iso9660"
        },
        "prefix": {
            "path": "/boot/grub2/"
        }
    }

--- stages/test/test_grub2_inst.py
+++ stages/test/test_grub2_inst.py
@@ -31,3 +31,22 @@ def test_grub2_partition(tmp_path, stage_module):
     with open(treedir / "disk.img", "rb") as f:
         msg = f.read(12)
         assert msg != b"Just testing"
+
+def test_grub2_iso9660(tmp_path, stage_module):
+    treedir = tmp_path / "tree"
+    os.makedirs(treedir, exist_ok=True)
+
+    options = {
+        "filename": "eltorito.img",
+        "platform": "i386-pc",
+        "core": {
+            "type": "mkimage",
+            "partlabel": "gpt",
+            "filesystem": "iso9660",
+        },
+        "prefix": {
+            "path": "/boot/grub2/",
+        },
+    }
+    stage_module.main(treedir, options)
+    assert os.path.exists(treedir / "eltorito.img")
