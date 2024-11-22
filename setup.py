4c2b8db0d7d0da0957097dbbc4360a1ec989d8c9
schutzbot <schutzbot@users.noreply.github.com>
2024-11-22 21:00:17 +0000

    Post release version bump
    [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="135",
+    version="136",
     description="A build system for OS images",
     packages=[
         "osbuild",
