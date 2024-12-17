139837f6285063b22565de903051a7c4bdc284fb
Tomáš Hozza <thozza@redhat.com>
2024-12-17 11:23:21 +0100

    manifest_tests.py: use osbuild-image-info from osbuild package
    Modify the test case to not use the image-info copy from manifest-db.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- test/cases/manifest_tests.sh
+++ test/cases/manifest_tests.sh
@@ -11,15 +11,13 @@ git clone "$MANIFEST_DB_REPO" manifest-db
 cd manifest-db
 git checkout --force "$MANIFEST_DB_COMMIT"
 
-# update selinux labels for the image-info tool
-OSBUILD_LABEL=$(matchpathcon -n /usr/bin/osbuild)
-chcon $OSBUILD_LABEL tools/image-info
-
 # set the maximum cache size to unlimited
 echo "{}" | sudo osbuild --store /var/lib/osbuild/store --cache-max-size unlimited -
 
 IFS='/' read -r -a array <<< $1
 
+sudo dnf install -y osbuild-tools
+
 # run the tests from the manifest-db for this arch+distro
 echo "Running the osbuild-image-test for arch $ARCH and ditribution $DISTRO_CODE"
-sudo tools/osbuild-image-test --arch=$ARCH --distro=$DISTRO_CODE --image-info-path=tools/image-info --instance-number="${array[0]}" --total-number-of-instances="${array[1]}"
+sudo tools/osbuild-image-test --arch=$ARCH --distro=$DISTRO_CODE --image-info-path="$(which osbuild-image-info)" --instance-number="${array[0]}" --total-number-of-instances="${array[1]}"
