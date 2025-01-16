0d25c845f82d9dec109605154e635aa46ee86400
Simon de Vlieger <supakeen@redhat.com>
2025-01-16 11:42:03 +0100

    stages: introduce `dnf.module-config`
    A new stage that allows writing a DNF module configuration file to a
    given path.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ stages/test/test_dnf_module_config.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import pytest
+
+from osbuild import testutil
+
+STAGE_NAME = "org.osbuild.dnf.module-config"
+
+
+@pytest.mark.parametrize("test_data,expected_err", [
+    # bad
+    ({"conf": "must-be-object"}, "'must-be-object' is not of type 'object'"),
+    ({"path": {}}, "{} is not of type 'string'"),
+    # good
+    ({
+        "conf":
+        {
+            "name": "some-module",
+            "stream": "some-stream",
+            "state": "some-state",
+            "profiles": ["some-profile"],
+        },
+    }, "")
+])
+def test_dnf_module_config_schema_validation(stage_schema, test_data, expected_err):
+    test_input = {
+        "type": STAGE_NAME,
+        "options": {
+        }
+    }
+    test_input["options"].update(test_data)
+    res = stage_schema.validate(test_input)
+
+    if expected_err == "":
+        assert res.valid is True, f"err: {[e.as_dict() for e in res.errors]}"
+    else:
+        assert res.valid is False
+        testutil.assert_jsonschema_error_contains(res, expected_err, expected_num_errs=1)
+
+
+def test_dnf_module_config_writes_file(tmp_path, stage_module):
+    treepath = tmp_path / "tree"
+    confpath = "etc/dnf/modules.d/module.conf"
+    fullpath = treepath / confpath
+
+    fullpath.parent.mkdir(parents=True, exist_ok=True)
+
+    options = {
+        "path": confpath,
+        "conf": {
+            "name": "some-module",
+            "stream": "some-stream",
+            "state": "some-state",
+            "profiles": ["some-profile", "other-profile"],
+        }
+    }
+
+    stage_module.main(treepath, options)
+
+    assert fullpath.exists()
+
+    confdata = fullpath.read_text()
+
+    assert confdata == """\
+[some-module]
+name = some-module
+stream = some-stream
+state = some-state
+profiles = some-profile, other-profile
+
+"""
