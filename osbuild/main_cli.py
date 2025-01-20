c31443f45b289eb9f0b5c729741bdecc6e59e9ab
Achilleas Koutsou <achilleas@koutsou.net>
2025-01-20 20:50:41 +0100

    osbuild/cli: add --cache as an alias for --store
    The name 'cache' is more recognisable and conveys the purpose of the
    option just as well (if not better) than 'store'.
    Also, the option that controls the size of this directory is called
    'cache-max-size', so we're already referring to it as a cache.

--- osbuild/main_cli.py
+++ osbuild/main_cli.py
@@ -69,7 +69,7 @@ def parse_arguments(sys_argv: List[str]) -> argparse.Namespace:
 
     parser.add_argument("manifest_path", metavar="MANIFEST",
                         help="json file containing the manifest that should be built, or a '-' to read from stdin")
-    parser.add_argument("--store", metavar="DIRECTORY", type=os.path.abspath,
+    parser.add_argument("--store", "--cache", metavar="DIRECTORY", type=os.path.abspath,
                         default=".osbuild",
                         help="directory where intermediary os trees are stored")
     parser.add_argument("-l", "--libdir", metavar="DIRECTORY", type=os.path.abspath, default="/usr/lib/osbuild",
