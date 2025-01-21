6abb959ed9b4cda14e43e3100579eadcce88ee60
Tomáš Hozza <thozza@redhat.com>
2025-01-21 13:42:58 +0100

    Add schutzbot/manifest-tests-install-deps.sh
    Add a script to setup the environment for manifest tests to run. This
    includes enabling specific repositories on RHEL, installing required
    packages and installing AWSCLI for accessing S3.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

---
+++ schutzbot/manifest-tests-install-deps.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Get OS details.
+source /etc/os-release
+ARCH=$(uname -m)
+
+# btrfs is not available on RHEL and CentOS.
+if [[ "${ID}" == "fedora" ]]; then
+    sudo dnf install -y btrfs-progs-devel
+fi
+
+sudo dnf -y install \
+    gcc \
+    git-core \
+    go \
+    gpgme-devel \
+    osbuild-depsolve-dnf \
+    osbuild-tools \
+    device-mapper-devel \
+    unzip
+
+# Install awscli
+WORKDIR=$(mktemp -d)
+pushd "${WORKDIR}"
+
+# Import the AWS CLI public key.
+AWSCLI_PUB_KEY="awscliv2.pubkey"
+cat <<EOF | tee "${AWSCLI_PUB_KEY}"
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBF2Cr7UBEADJZHcgusOJl7ENSyumXh85z0TRV0xJorM2B/JL0kHOyigQluUG
+ZMLhENaG0bYatdrKP+3H91lvK050pXwnO/R7fB/FSTouki4ciIx5OuLlnJZIxSzx
+PqGl0mkxImLNbGWoi6Lto0LYxqHN2iQtzlwTVmq9733zd3XfcXrZ3+LblHAgEt5G
+TfNxEKJ8soPLyWmwDH6HWCnjZ/aIQRBTIQ05uVeEoYxSh6wOai7ss/KveoSNBbYz
+gbdzoqI2Y8cgH2nbfgp3DSasaLZEdCSsIsK1u05CinE7k2qZ7KgKAUIcT/cR/grk
+C6VwsnDU0OUCideXcQ8WeHutqvgZH1JgKDbznoIzeQHJD238GEu+eKhRHcz8/jeG
+94zkcgJOz3KbZGYMiTh277Fvj9zzvZsbMBCedV1BTg3TqgvdX4bdkhf5cH+7NtWO
+lrFj6UwAsGukBTAOxC0l/dnSmZhJ7Z1KmEWilro/gOrjtOxqRQutlIqG22TaqoPG
+fYVN+en3Zwbt97kcgZDwqbuykNt64oZWc4XKCa3mprEGC3IbJTBFqglXmZ7l9ywG
+EEUJYOlb2XrSuPWml39beWdKM8kzr1OjnlOm6+lpTRCBfo0wa9F8YZRhHPAkwKkX
+XDeOGpWRj4ohOx0d2GWkyV5xyN14p2tQOCdOODmz80yUTgRpPVQUtOEhXQARAQAB
+tCFBV1MgQ0xJIFRlYW0gPGF3cy1jbGlAYW1hem9uLmNvbT6JAlQEEwEIAD4CGwMF
+CwkIBwIGFQoJCAsCBBYCAwECHgECF4AWIQT7Xbd/1cEYuAURraimMQrMRnJHXAUC
+ZqFYbwUJCv/cOgAKCRCmMQrMRnJHXKYuEAC+wtZ611qQtOl0t5spM9SWZuszbcyA
+0xBAJq2pncnp6wdCOkuAPu4/R3UCIoD2C49MkLj9Y0Yvue8CCF6OIJ8L+fKBv2DI
+yWZGmHL0p9wa/X8NCKQrKxK1gq5PuCzi3f3SqwfbZuZGeK/ubnmtttWXpUtuU/Iz
+VR0u/0sAy3j4uTGKh2cX7XnZbSqgJhUk9H324mIJiSwzvw1Ker6xtH/LwdBeJCck
+bVBdh3LZis4zuD4IZeBO1vRvjot3Oq4xadUv5RSPATg7T1kivrtLCnwvqc6L4LnF
+0OkNysk94L3LQSHyQW2kQS1cVwr+yGUSiSp+VvMbAobAapmMJWP6e/dKyAUGIX6+
+2waLdbBs2U7MXznx/2ayCLPH7qCY9cenbdj5JhG9ibVvFWqqhSo22B/URQE/CMrG
++3xXwtHEBoMyWEATr1tWwn2yyQGbkUGANneSDFiTFeoQvKNyyCFTFO1F2XKCcuDs
+19nj34PE2TJilTG2QRlMr4D0NgwLLAMg2Los1CK6nXWnImYHKuaKS9LVaCoC8vu7
+IRBik1NX6SjrQnftk0M9dY+s0ZbAN1gbdjZ8H3qlbl/4TxMdr87m8LP4FZIIo261
+Eycv34pVkCePZiP+dgamEiQJ7IL4ZArio9mv6HbDGV6mLY45+l6/0EzCwkI5IyIf
+BfWC9s/USgxchg==
+=ptgS
+-----END PGP PUBLIC KEY BLOCK-----
+EOF
+gpg --import "$AWSCLI_PUB_KEY"
+
+# Download the AWS CLI v2 binary and its signature.
+AWSCLI_ARCHIVE="awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}.zip" -o "${AWSCLI_ARCHIVE}"
+AWSCLI_SIG="awscliv2.sig"
+curl  "https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}.zip.sig" -o "${AWSCLI_SIG}"
+
+# Verify the signature.
+gpg --verify "${AWSCLI_SIG}" "${AWSCLI_ARCHIVE}"
+
+# Unzip the AWS CLI v2 binary and install it.
+unzip "${AWSCLI_ARCHIVE}"
+sudo ./aws/install 1> /dev/null
+
+# Verify the installation.
+aws --version
+popd
+rm -rf "${WORKDIR}"
