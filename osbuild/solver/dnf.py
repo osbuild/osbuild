ec8f729e79cf812d3b6367a9f5d19329263c3a25
Simon de Vlieger <supakeen@redhat.com>
2025-01-23 15:44:20 +0100

    depsolve: remove `nevra` field
    The NEVRA field accidentally made it into the public API. It shouldn't
    be as it is not used downstream and in fact breaks downstream.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- osbuild/solver/dnf.py
+++ osbuild/solver/dnf.py
@@ -302,7 +302,6 @@ class DNF(SolverBase):
         pkg_repos = {}
         for package in last_transaction:
             packages.append({
-                "nevra": f"{package.name}-{package.evr}.{package.arch}",
                 "name": package.name,
                 "epoch": package.epoch,
                 "version": package.version,
@@ -360,7 +359,15 @@ class DNF(SolverBase):
             if transaction.get("module-enable-specs") or modules_in_package_specs:
                 # we'll be checking later if any packages-from-modules are in the
                 # packages-to-install set so let's do this only once here
-                package_nevras = list(p["nevra"] for p in packages)
+                package_nevras = []
+
+                for package in packages:
+                    if package["epoch"] == 0:
+                        package_nevras.append(
+                            f"{package['name']}-{package['version']}-{package['release']}.{package['arch']}")
+                    else:
+                        package_nevras.append(
+                            f"{package['name']}-{package['epoch']}:{package['version']}-{package['release']}.{package['arch']}")
 
                 for module_spec in itertools.chain(
                     transaction.get("module-enable-specs", []),
