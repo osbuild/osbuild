a095f2af456dc56b5791c79163f6f10fce2b0320
Tomáš Hozza <thozza@redhat.com>
2025-04-01 12:36:18 +0200

    Set the release version to 141.2
    I forgot to bump the version to 141.1 before making the v141.1 release.
    Also, the release action's script that bumps the version did not
    support the dot releases. This should be fixed soon.
    
    The version is always set to the "next" to be released version and it is
    bumped after we make a release by the GH action.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="141",
+    version="141.2",
     description="A build system for OS images",
     packages=[
         "osbuild",
