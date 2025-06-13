89ab7e0c4aa39e721415335501b875d2c9a9fe67
Karolina Surma <ksurma@redhat.com>
2025-06-13 14:08:05 +0200

    Skip x86_64-specific tests on different architectures

--- test/mod/test_testutil_dnf4.py
+++ test/mod/test_testutil_dnf4.py
@@ -1,10 +1,12 @@
 import os.path
+import platform
 
 import pytest
 
 testutil_dnf4 = pytest.importorskip("osbuild.testutil.dnf4")
 
 
+@pytest.mark.skipif(platform.machine() != "x86_64", reason="Test data contain only x86_64 arched pkgs")
 def test_depsolve_pkgset():
     pkgset = testutil_dnf4.depsolve_pkgset([os.path.abspath("./test/data/testrepos/baseos")], ["bash"])
     assert len(pkgset) == 15
