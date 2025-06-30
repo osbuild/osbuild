b87ddf0c05afbfac0c7e4df0ee756a5087e2ca87
Simon de Vlieger <supakeen@redhat.com>
2025-06-30 16:51:27 +0200

    stages/gzip: introduce a gz stage
    We have an `xz`, and `zstd` stage to do compression with, which are
    being used. We don't have a `gzip` stage to do the same for gz. Let's
    add it so we can generalise our compression between disk images and
    tarballs.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- stages/test/test_gzip.py
+++ stages/test/test_gzip.py
@@ -1,41 +1,87 @@
 #!/usr/bin/python3
 
-from unittest.mock import patch
+import os.path
+import subprocess
+from unittest import mock
 
 import pytest
+
+from osbuild import testutil
+from osbuild.testutil import has_executable, make_fake_input_tree
 
 STAGE_NAME = "org.osbuild.gzip"
 
 
-@pytest.mark.parametrize("test_options,expected_level", [
-    # our default
-    ({}, "-1"),
-    # custom settings
-    ({"level": "1"}, "-1"),
-    ({"level": "6"}, "-6"),
-    ({"level": "9"}, "-9"),
+@pytest.mark.parametrize("test_data,expected_err", [
+    # bad
+    ({}, "'filename' is a required property"),
+    # good
+    ({"filename": "image.gz"}, ""),
 ])
-@patch("subprocess.run")
-def test_gzip_compression_default_to_level1(mocked_run, tmp_path, stage_module, test_options, expected_level):
-    inp = {
+def test_schema_validation_gzip(stage_schema, test_data, expected_err):
+    test_input = {
+        "type": STAGE_NAME,
+        "options": {
+        }
+    }
+    test_input["options"].update(test_data)
+    res = stage_schema.validate(test_input)
+
+    if expected_err == "":
+        assert res.valid is True, f"err: {[e.as_dict() for e in res.errors]}"
+    else:
+        assert res.valid is False
+        testutil.assert_jsonschema_error_contains(res, expected_err, expected_num_errs=1)
+
+
+@pytest.fixture(name="fake_input_tree")
+def fake_input(tmp_path):
+    fake_input_tree = make_fake_input_tree(tmp_path, {
+        "/sha256:ff3e910ceb79fd66e67b7ff4a9073b1d584c748a5f3d1e6b1873afa1cf35ec59": "A file to be compressed",
+    })
+    inputs = {
         "file": {
-            "path": "/input/file/path",
+            "path": fake_input_tree,
             "data": {
                 "files": {
-                    "hash:value": "some-file",
+                    "sha256:ff3e910ceb79fd66e67b7ff4a9073b1d584c748a5f3d1e6b1873afa1cf35ec59": {}
                 }
-            },
-        },
+            }
+        }
     }
-    output = tmp_path
-    options = {
-        "filename": "out.tar.gz",
-    }
-    options.update(test_options)
+    return (fake_input_tree, inputs)
 
-    stage_module.main(inp, output, options)
-    assert len(mocked_run.call_args_list) == 1
-    args, kwargs = mocked_run.call_args_list[0]
-    assert args == (["gzip", "--no-name", "--stdout", expected_level, "/input/file/path/hash:value"],)
-    assert kwargs["check"]
-    assert kwargs["stdout"].name.endswith("out.tar.gz")
+
+@pytest.mark.skipif(not has_executable("gzip"), reason="no gzip executable")
+def test_gzip_integration(tmp_path, stage_module, fake_input_tree):  # pylint: disable=unused-argument
+    inputs = fake_input_tree[1]
+    options = {
+        "filename": "image.txt.gz",
+    }
+    stage_module.main(inputs, tmp_path, options)
+
+    img_path = os.path.join(tmp_path, "image.txt.gz")
+    assert os.path.exists(img_path)
+    output = subprocess.check_output([
+        "gzip", "--decompress", "--stdout", img_path], encoding="utf-8")
+    assert "A file to be compressed" in output
+
+
+@mock.patch("subprocess.run")
+def test_gzip_cmdline(mock_run, tmp_path, stage_module, fake_input_tree):
+    fake_input_path = fake_input_tree[0]
+    inputs = fake_input_tree[1]
+    filename = "image.txt.gz"
+    options = {
+        "filename": filename,
+    }
+    stage_module.main(inputs, tmp_path, options)
+
+    expected = [
+        "gzip",
+        "--keep",
+        "--stdout",
+        "-6",
+        os.path.join(fake_input_path, "sha256:ff3e910ceb79fd66e67b7ff4a9073b1d584c748a5f3d1e6b1873afa1cf35ec59")
+    ]
+    mock_run.assert_called_with(expected, stdout=mock.ANY, check=True)
