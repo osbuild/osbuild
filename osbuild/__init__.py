4ec94759a1fd3350af311ae966fa4035247c828a
schutzbot <schutzbot@users.noreply.github.com>
2025-07-02 08:36:52 +0000

    Post release version bump
    [skip ci]

--- osbuild/__init__.py
+++ osbuild/__init__.py
@@ -10,7 +10,7 @@ independent of osbuild but used across the osbuild codebase.
 
 from .pipeline import Manifest, Pipeline, Stage
 
-__version__ = "154"
+__version__ = "155"
 
 __all__ = [
     "Manifest",
