f67c1e4df8f43c8a853a8c4c2ae98998b6f5b863
Michael Vogt <michael.vogt@gmail.com>
2025-09-11 11:12:06 +0200

    test: add test that ensures osbuild can be run with plain podman
    This test ensures that we can build manifests that do not require
    access to loop devices with with rootless podman. We still need
    CAP_SYS_ADMIN, CAP_NET_ADMIN inside the container but that is
    much less than the previous required rootfull podman.
    
    Note that for non-selinux systems we need to apply a workaround
    for the test here we will not be able to build
    org.osbuild.selinux with a rootless container because selinux
    labeling will not work in a user namespace.

---
+++ test/run/test_less_privs.py
@@ -0,0 +1,74 @@
+import os
+import random
+import shutil
+import string
+import subprocess
+import warnings
+from contextlib import contextmanager
+from pathlib import Path
+
+import pytest
+
+
+def has_selinux_lsm():
+    lsms_path = Path("/sys/kernel/security/lsm")
+    # if unsure, assume no selinux that is always safe
+    if not lsms_path.exists():
+        return False
+    lsms = lsms_path.read_text("utf8").split(",")
+    return "selinux" in lsms
+
+
+@contextmanager
+def osbuild_container():
+    container_tag = "osbuild-test-" + "".join(random.choices(string.digits, k=12))
+    cntfile_path = Path(__file__).parent / "../../Containerfile"
+    subprocess.check_call([
+        "podman", "build",
+        "--cache-ttl=0",
+        "-t", container_tag,
+        "-f", cntfile_path], encoding="utf8")
+    yield container_tag
+    subprocess.check_call(["podman", "rmi", container_tag])
+
+
+@pytest.mark.skipif(os.getuid() != 0, reason="must run as user")
+@pytest.mark.skipif(not shutil.which("podman"), reason="needs podman to run")
+def test_osbuild_works_with_less_privs_pr2187(tmp_path):
+    # the container manifest does not require loopback mounts so we
+    # use it for this test
+    manifest_in_path = Path(__file__).parent / "../data/manifests/fedora-container.json"
+    manifest_out_path = manifest_in_path
+
+    # On a non-selinux system (like Ubuntu in the GH runners) labeling
+    # in a user-namespace will always error with EPERM. This works
+    # fine however on a selinux system even if selinux is disabled via
+    # SELINUX=disabled in /etc/selinux/config (i.e. selinux getting
+    # initialized in the kernel config is enough).
+    # See https://github.com/torvalds/linux/blob/v6.16/security/selinux/hooks.c#L3293
+    if not has_selinux_lsm():
+        warnings.warn("no selinux detected, applying workaround")
+        manifest_no_selinux = manifest_in_path.read_text("utf8").replace(
+            "org.osbuild.selinux", "org.osbuild.noop")
+        manifest_out_path = tmp_path / "manifest_no_selinux.json"
+        manifest_out_path.write_text(manifest_no_selinux, "utf8")
+
+    # build container
+    with osbuild_container() as cnt_tag:
+        subprocess.check_call([
+            "podman", "run", "--rm",
+            # we need this for bind mounts
+            "--cap-add=SYS_ADMIN",
+            # bubble wrap needs this curently
+            "--cap-add=NET_ADMIN",
+            # tweak selinux
+            "--security-opt", "label=disable",
+            # volumes
+            "-v", f"{manifest_out_path}:/manifest.json",
+            "-v", f"{tmp_path}:/output",
+            cnt_tag,
+            "/manifest.json",
+            "--export", "container",
+            "--output-directory", "/output",
+        ])
+    assert (tmp_path / "container" / "fedora-container.tar").exists()
