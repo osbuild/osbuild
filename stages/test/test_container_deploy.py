5ff3e403868955cff2bccc0fa7335bda31e47d9e
Dusty Mabe <dusty@dustymabe.com>
2025-10-15 15:18:43 -0400

    drop remove_signatures from org.osbuild.container-deploy
    The original reason to implement this for org.osbuild.container-deploy
    was because we were doing `skopeo copy` from one containers-storage
    instance to another and we needed the option if the container was
    signed.
    
    With the most recent commit to container_mount in containers.py we
    don't actually ever skopeo copy from one containers-storage instance
    to another, but rather `podman mount` directly, so we should be able
    to drop this option.
    
    Context in:
    - https://github.com/containers/container-libs/issues/197
    - https://github.com/osbuild/bootc-image-builder/issues/681
    - https://github.com/osbuild/osbuild/pull/1906

--- stages/test/test_container_deploy.py
+++ stages/test/test_container_deploy.py
@@ -7,7 +7,6 @@ from unittest.mock import call, patch
 
 import pytest
 
-import osbuild.testutil
 from osbuild.testutil import has_executable, make_container, make_fake_images_inputs
 
 STAGE_NAME = "org.osbuild.container-deploy"
@@ -76,38 +75,3 @@ def test_container_deploy_exclude(tmp_path, stage_module):
     assert (output_dir / "dir1/file3").read_bytes() == b"dir1/file3 content"
     assert not (output_dir / "dir2/file4").exists()
     assert not (output_dir / "dir2").exists()
-
-
-@pytest.mark.skipif(os.getuid() != 0, reason="needs root")
-@pytest.mark.skipif(not has_executable("podman"), reason="no podman executable")
-@pytest.mark.parametrize("remove_signatures", [True, False])
-def test_remove_signatures(tmp_path, stage_module, remove_signatures):
-    with make_container(tmp_path, {}) as cont_tag:
-        # export for the container-deploy stage
-        fake_oci_path = tmp_path / "fake-container"
-        subprocess.check_call([
-            "podman", "save",
-            "--format=oci-archive",
-            f"--output={fake_oci_path}",
-            cont_tag,
-        ])
-    inputs = make_fake_images_inputs(fake_oci_path, "some-name")
-
-    fake_skopeo = textwrap.dedent("""\
-    #! /bin/sh
-    /usr/bin/skopeo $@
-    """)
-
-    with osbuild.testutil.mock_command("skopeo", fake_skopeo) as args:
-        inputs = make_fake_images_inputs(fake_oci_path, "some-name")
-        options = {
-            "remove-signatures": remove_signatures,
-        }
-        output_dir = tmp_path / "output"
-        stage_module.main(inputs, output_dir, options)
-        if remove_signatures:
-            # Check that skopeo has --remove-signatures right after the copy subcommand
-            assert args.call_args_list[0][0:2] == ["copy", "--remove-signatures"]
-        else:
-            # Check that --remove-signatures is not present in the skopeo command
-            assert "--remove-signatures" not in args.call_args_list[0]
