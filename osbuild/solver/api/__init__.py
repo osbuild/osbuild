bf2f42159b7bab6fd83a03470324382f2b32c42f
Tomáš Hozza <thozza@redhat.com>
2025-11-13 09:48:57 +0100

    osbuild/solver/api: add V1 parser for the Solver request
    Add V1 API parser for the Solver request. This is the base for
    decoupling the Solver response JSON representation from the internal
    request data classes. It will make it possible to specify the API
    version in the request and also support different versions of the Solver
    request at the same time.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- osbuild/solver/api/__init__.py
+++ osbuild/solver/api/__init__.py
@@ -3,7 +3,9 @@ from enum import Enum
 from types import ModuleType
 from typing import Any, Dict, List, Optional
 
+from osbuild.solver import InvalidRequestError
 from osbuild.solver.model import Package, Repository
+from osbuild.solver.request import SolverRequest
 
 
 class SolverAPIVersion(Enum):
@@ -18,6 +20,27 @@ def get_api_module(api_version: SolverAPIVersion) -> ModuleType:
         return importlib.import_module(f"osbuild.solver.api.v{api_version.value}")
     except ImportError as e:
         raise ValueError(f"Invalid solver API version: {api_version}") from e
+
+
+def parse_request(request_dict: Dict) -> SolverRequest:
+    """
+    Parse a request dict into a SolverRequest object
+
+    Detects API version from the request (defaults to V1) and
+    uses the appropriate version-specific parser.
+    """
+    # Get API version from the request, default to V1, because the current (V1)
+    # API implementation does not set any version field.
+    # pylint: disable=fixme
+    # XXX: remove the default and make it an error once V1 is deprecated.
+    version_num = request_dict.get("api_version", 1)
+    try:
+        api_version = SolverAPIVersion(version_num)
+    except ValueError as e:
+        raise InvalidRequestError(f"Invalid API version: {e}") from e
+
+    api_module = get_api_module(api_version)
+    return api_module.parse_request(request_dict)
 
 
 def serialize_response_depsolve(
