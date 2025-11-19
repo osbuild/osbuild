f429075d291dc5358a6ca2dce2ed3dc3a8d56ac7
schutzbot <schutzbot@users.noreply.github.com>
2025-11-19 08:36:43 +0000

    Post release version bump
    [skip ci]

--- osbuild.spec
+++ osbuild.spec
@@ -1,7 +1,7 @@
 %global         forgeurl https://github.com/osbuild/osbuild
 %global         selinuxtype targeted
 
-Version:        165
+Version:        166
 
 %forgemeta
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="osbuild",
-    version="165",
+    version="166",
     description="A build system for OS images",
     packages=[
         "osbuild",
