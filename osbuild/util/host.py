c935b6f1f9f6200ea40cc573f2e0fd84efe498d1
Alexander Larsson <alexl@redhat.com>
2026-01-27 11:06:54 +0100

    Add workaround to make osbuild.utils.host importable if toml is missing
    This was causing problems in the unit tests for the stages. It was
    triggering an import of ostree.host from osbuild.qemu, and the host
    import failed because it failed to import toml, as the test had no
    toml module in the build pipeline.
    
    We work around this by delaying the import until use, which will not
    actually happen in the CI test.

--- osbuild/util/host.py
+++ osbuild/util/host.py
@@ -3,13 +3,22 @@ Utility functions that only run on the host (osbuild internals or host modules l
 
 These should not be used by stages or code that runs in the build root.
 """
-from osbuild.util import toml
 
 
 def get_container_storage():
     """
     Read the host storage configuration.
     """
+
+    # In some cases (for example in CI) the toml module is not available.
+    # In such cases, we error at use, not at import time to avoid breaking things.
+
+    try:
+        # pylint: disable=import-outside-toplevel
+        from osbuild.util import toml
+    except ImportError as e:
+        raise FileNotFoundError("could not find toml parser to read container storage configuration") from e
+
     config_paths = ("/etc/containers/storage.conf", "/usr/share/containers/storage.conf")
     for conf_path in config_paths:
         try:
