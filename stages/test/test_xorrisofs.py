acb4ef6c069b75c280b1973832a8b86c6accc903
Simon de Vlieger <cmdr@supakeen.com>
2026-02-17 08:20:14 +0100

    stage/xorrisofs: accept inputs
    Allow for the `efi` argument to be a URL that either refers to the tree
    if it has no prefix; or perhaps an input if necessary.
    
    This allows us to pull the EFI image from elsewhere that's not in the
    tree thus not including it in the ISO and also allowing us to have a
    sepearate pipeline in `images` that produces the image.
    
    Note the special handling for the syslinux branch of this stage. It
    never used an appended partition so for backwards compatibility we keep
    using an rr_path (rock-ridge, on the ISO) when the input for the efiboot
    image is actually in the tree.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- stages/test/test_xorrisofs.py
+++ stages/test/test_xorrisofs.py
@@ -20,8 +20,10 @@ def test_xorrisofs_syslinux_efi_from_tree(mock_run, tmp_path, stage_module):
 
     # default (syslinux)
     stage_module.main(
-        inputs,
-        tmp_path,
+        {
+            "inputs": inputs,
+            "tree": tmp_path,
+        },
         {
             "boot": {
                 "image": "images/eltorito.img",
@@ -33,6 +35,11 @@ def test_xorrisofs_syslinux_efi_from_tree(mock_run, tmp_path, stage_module):
         },
     )
 
+    # note that in the syslinux case we *don't* have an absolute path
+    # for an in-tree efiboot.img; this is to keep backwards compatibility
+    # in the stage as it never used an appended partition in the syslinux
+    # case.
+    # it now only does so if the efi image actually comes from not-the-tree
     mock_run.assert_called_with([
         "/usr/bin/xorrisofs",
         "-verbose",
@@ -64,8 +71,10 @@ def test_xorrisofs_grub2_efi_from_tree(mock_run, tmp_path, stage_module):
     }
 
     stage_module.main(
-        inputs,
-        tmp_path,
+        {
+            "inputs": inputs,
+            "tree": tmp_path,
+        },
         {
             "boot": {
                 "image": "images/eltorito.img",
@@ -88,6 +97,117 @@ def test_xorrisofs_grub2_efi_from_tree(mock_run, tmp_path, stage_module):
         "-partition_offset", "16",
         "-appended_part_as_gpt",
         "-append_partition", "2", "C12A7328-F81F-11D2-BA4B-00A0C93EC93B", os.path.join(fake_input_tree, "images/efiboot.img"),
+        "-iso_mbr_part_type", "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7",
+        "-b", "images/eltorito.img",
+        "-c", "boot.cat",
+        "--boot-catalog-hide",
+        "-no-emul-boot",
+        "-boot-load-size", "4",
+        "-boot-info-table",
+        "--grub2-boot-info",
+        "-eltorito-alt-boot",
+        "-e", "--interval:appended_partition_2:all::",
+        "-no-emul-boot",
+        "-o", os.path.join(tmp_path, "test.iso"),
+        fake_input_tree,
+    ], check=True)
+
+
+@mock.patch("subprocess.run")
+def test_xorrisofs_syslinux_efi_from_input(mock_run, tmp_path, stage_module):
+    fake_input_tree = make_fake_input_tree(tmp_path, {})
+    fake_input_tree_efi = make_fake_input_tree(tmp_path, {}, name="efi")
+
+    inputs = {
+        "tree": {
+            "path": fake_input_tree,
+        },
+        "efi": {
+            "path": fake_input_tree_efi,
+        }
+    }
+
+    # default (syslinux)
+    stage_module.main(
+        {
+            "inputs": inputs,
+            "tree": tmp_path,
+        },
+        {
+            "boot": {
+                "image": "images/eltorito.img",
+                "catalog": "boot.cat",
+            },
+            "filename": "test.iso",
+            "volid": "test",
+            "efi": "input://efi/efiboot.img",
+        },
+    )
+
+    mock_run.assert_called_with([
+        "/usr/bin/xorrisofs",
+        "-verbose",
+        "-V", "test",
+        "-b", "images/eltorito.img",
+        "-c", "boot.cat",
+        "--boot-catalog-hide",
+        "-boot-load-size", "4",
+        "-boot-info-table",
+        "-no-emul-boot",
+        "-rock",
+        "-joliet",
+        "-partition_offset", "16",
+        "-appended_part_as_gpt",
+        "-append_partition", "2", "C12A7328-F81F-11D2-BA4B-00A0C93EC93B", os.path.join(fake_input_tree_efi, "efiboot.img"),
+        "-eltorito-alt-boot",
+        "-e", "--interval:appended_partition_2:all::",
+        "-no-emul-boot",
+        "-o", os.path.join(tmp_path, "test.iso"),
+        fake_input_tree,
+    ], check=True)
+
+
+@mock.patch("subprocess.run")
+def test_xorrisofs_grub2_efi_from_input(mock_run, tmp_path, stage_module):
+    fake_input_tree = make_fake_input_tree(tmp_path, {})
+    fake_input_tree_efi = make_fake_input_tree(tmp_path, {}, name="efi")
+
+    inputs = {
+        "tree": {
+            "path": fake_input_tree,
+        },
+        "efi": {
+            "path": fake_input_tree_efi,
+        }
+    }
+
+    stage_module.main(
+        {
+            "inputs": inputs,
+            "tree": tmp_path,
+        },
+        {
+            "boot": {
+                "image": "images/eltorito.img",
+                "catalog": "boot.cat",
+            },
+            "grub2mbr": "/usr/lib/grub/i386-pc/boot_hybrid.img",
+            "filename": "test.iso",
+            "volid": "test",
+            "efi": "input://efi/efiboot.img",
+        },
+    )
+
+    mock_run.assert_called_with([
+        "/usr/bin/xorrisofs",
+        "-verbose",
+        "-rock",
+        "-joliet",
+        "-V", "test",
+        "--grub2-mbr", "/usr/lib/grub/i386-pc/boot_hybrid.img",
+        "-partition_offset", "16",
+        "-appended_part_as_gpt",
+        "-append_partition", "2", "C12A7328-F81F-11D2-BA4B-00A0C93EC93B", os.path.join(fake_input_tree_efi, "efiboot.img"),
         "-iso_mbr_part_type", "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7",
         "-b", "images/eltorito.img",
         "-c", "boot.cat",
