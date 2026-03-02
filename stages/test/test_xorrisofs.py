6ba619f2eea162cd24e96bd7df327467717c34e2
Simon de Vlieger <cmdr@supakeen.com>
2026-03-02 19:29:11 +0100

    stage/xorrisofs: support excluding files
    In another PR [1], [2] I wanted to introduce efiboot.img being sourced from
    inputs. However, this was quite a larger sweeping change that might need
    more careful consideration (we might use such a change to also switch to
    using `xorriso` directly, and to revisit all of our ISO mushing needs).
    
    This PR instead introduces the option to exclude certain files. See the
    documentation for `xorrisofs` for the options but it's basically a shell
    glob. If it doesn't contain a `/` then the basename of the file is used
    instead.
    
    The change allows to achieve the goal of not including `efiboot.img`
    without complicating matters.
    
    [1]: https://github.com/osbuild/osbuild/pull/2352
    [2]: https://github.com/osbuild/images/pull/2204
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- stages/test/test_xorrisofs.py
+++ stages/test/test_xorrisofs.py
@@ -55,6 +55,55 @@ def test_xorrisofs_syslinux(mock_run, tmp_path, stage_module):
 
 
 @mock.patch("subprocess.run")
+def test_xorrisofs_syslinux_with_excludes(mock_run, tmp_path, stage_module):
+    fake_input_tree = make_fake_input_tree(tmp_path, {})
+
+    inputs = {
+        "tree": {
+            "path": fake_input_tree,
+        }
+    }
+
+    # default (syslinux)
+    stage_module.main(
+        inputs,
+        tmp_path,
+        {
+            "boot": {
+                "image": "images/eltorito.img",
+                "catalog": "boot.cat",
+            },
+            "filename": "test.iso",
+            "volid": "test",
+            "efi": "images/efiboot.img",
+            "exclude": [
+                "efiboot.img",
+            ],
+        },
+    )
+
+    mock_run.assert_called_with([
+        "/usr/bin/xorrisofs",
+        "-verbose",
+        "-V", "test",
+        "-b", "images/eltorito.img",
+        "-c", "boot.cat",
+        "--boot-catalog-hide",
+        "-boot-load-size", "4",
+        "-boot-info-table",
+        "-no-emul-boot",
+        "-rock",
+        "-joliet",
+        "-eltorito-alt-boot",
+        "-e", "images/efiboot.img",
+        "-no-emul-boot",
+        "-exclude", "efiboot.img",
+        "-o", os.path.join(tmp_path, "test.iso"),
+        fake_input_tree,
+    ], check=True)
+
+
+@mock.patch("subprocess.run")
 @mock.patch("os.path.exists")
 def test_xorrisofs_grub2(mock_exists, mock_run, tmp_path, stage_module):
     mock_exists.return_value = True
@@ -103,6 +152,64 @@ def test_xorrisofs_grub2(mock_exists, mock_run, tmp_path, stage_module):
         "-eltorito-alt-boot",
         "-e", "--interval:appended_partition_2:all::",
         "-no-emul-boot",
+        "-o", os.path.join(tmp_path, "test.iso"),
+        fake_input_tree,
+    ], check=True)
+
+
+@mock.patch("subprocess.run")
+@mock.patch("os.path.exists")
+def test_xorrisofs_grub2_with_excludes(mock_exists, mock_run, tmp_path, stage_module):
+    mock_exists.return_value = True
+
+    fake_input_tree = make_fake_input_tree(tmp_path, {})
+
+    inputs = {
+        "tree": {
+            "path": fake_input_tree,
+        }
+    }
+
+    stage_module.main(
+        inputs,
+        tmp_path,
+        {
+            "boot": {
+                "image": "images/eltorito.img",
+                "catalog": "boot.cat",
+            },
+            "grub2mbr": "/usr/lib/grub/i386-pc/boot_hybrid.img",
+            "filename": "test.iso",
+            "volid": "test",
+            "efi": "images/efiboot.img",
+            "exclude": [
+                "efiboot.img",
+            ],
+        },
+    )
+
+    mock_run.assert_called_with([
+        "/usr/bin/xorrisofs",
+        "-verbose",
+        "-rock",
+        "-joliet",
+        "-V", "test",
+        "--grub2-mbr", "/usr/lib/grub/i386-pc/boot_hybrid.img",
+        "-partition_offset", "16",
+        "-appended_part_as_gpt",
+        "-append_partition", "2", "C12A7328-F81F-11D2-BA4B-00A0C93EC93B", os.path.join(fake_input_tree, "images/efiboot.img"),
+        "-iso_mbr_part_type", "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7",
+        "-b", "images/eltorito.img",
+        "-c", "boot.cat",
+        "--boot-catalog-hide",
+        "-no-emul-boot",
+        "-boot-load-size", "4",
+        "-boot-info-table",
+        "--grub2-boot-info",
+        "-eltorito-alt-boot",
+        "-e", "--interval:appended_partition_2:all::",
+        "-no-emul-boot",
+        "-exclude", "efiboot.img",
         "-o", os.path.join(tmp_path, "test.iso"),
         fake_input_tree,
     ], check=True)
