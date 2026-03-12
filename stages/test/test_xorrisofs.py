f5930c09e4f2e2d07051c0998db2ac99e7d44c73
Brian C. Lane <bcl@redhat.com>
2026-03-12 13:27:52 -0700

    xorrisofs: Add option to disable boot-info arguments
    On s390x the syslinux function needs to skip the -boot-info-table
    option since it is not a grub2 or syslinux bootloader. This adds the
    "nobootinfo" switch to the "boot" section. It also effects the grub2
    function, skipping -boot-info-table and --grub2-boot-info.
    
    These options overwrite parts of the image file passed to -b and can
    only be used with syslinux and grub2 boot images.
    
    Includes a test for the new behavior.
    
    Related: HMS-10110

--- stages/test/test_xorrisofs.py
+++ stages/test/test_xorrisofs.py
@@ -106,6 +106,52 @@ def test_xorrisofs_syslinux_with_excludes(mock_run, tmp_path, stage_module):
 
 
 @mock.patch("subprocess.run")
+def test_xorrisofs_syslinux_nobootinfo(mock_run, tmp_path, stage_module):
+
+    fake_input_tree = make_fake_input_tree(tmp_path, {})
+
+    inputs = {
+        "tree": {
+            "path": fake_input_tree,
+        }
+    }
+
+    # default (syslinux)
+    stage_module.main(
+        inputs,
+        tmp_path,
+        {
+            "boot": {
+                "image": "images/eltorito.img",
+                "catalog": "boot.cat",
+                "nobootinfo": True,
+            },
+            "filename": "test.iso",
+            "volid": "test",
+            "efi": "images/efiboot.img",
+        },
+    )
+
+    mock_run.assert_called_with([
+        "/usr/bin/xorrisofs",
+        "-verbose",
+        "-V", "test",
+        "-b", "images/eltorito.img",
+        "-c", "boot.cat",
+        "--boot-catalog-hide",
+        "-boot-load-size", "4",
+        "-no-emul-boot",
+        "-rock",
+        "-joliet",
+        "-eltorito-alt-boot",
+        "-e", "images/efiboot.img",
+        "-no-emul-boot",
+        "-o", os.path.join(tmp_path, "test.iso"),
+        fake_input_tree,
+    ], check=True)
+
+
+@mock.patch("subprocess.run")
 @mock.patch("os.path.exists")
 def test_xorrisofs_grub2(mock_exists, mock_run, tmp_path, stage_module):
     mock_exists.return_value = True
